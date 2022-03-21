3b0344420eeb8db92701a0b449b3ce1242ef5f89
Jeremy Rea <jeremyrea@fastmail.com>
2022-03-21 12:56:00 -0400

    Stick header to top and add sidebar menu

--- lib/genialais_web/views/layout_view.ex
+++ lib/genialais_web/views/layout_view.ex
@@ -9,4 +9,22 @@ defmodule GenialaisWeb.LayoutView do
       "🇫🇷": "fr"
     ]
   end
+
+  def active_class(conn, path) do
+    current_path = Path.join(["/" | conn.path_info])
+    if path == current_path do
+      "active"
+    else
+      nil
+    end
+  end
+
+  def active_link(conn, text, opts) do
+    class = [opts[:class], active_class(conn, opts[:to])]
+            |> Enum.filter(& &1) 
+            |> Enum.join(" ")
+    opts = opts
+          |> Keyword.put(:class, class)
+    link(text, opts)
+  end
 end
