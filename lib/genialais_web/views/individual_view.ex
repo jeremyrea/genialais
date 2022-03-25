1cb747a15776d67b934e5d9a9240e5a3a8262baf
Jeremy Rea <jeremyrea@fastmail.com>
2022-03-25 17:16:56 -0400

    Reorder page_title functions

--- lib/genialais_web/views/individual_view.ex
+++ lib/genialais_web/views/individual_view.ex
@@ -1,6 +1,6 @@
 defmodule GenialaisWeb.IndividualView do
   use GenialaisWeb, :view
 
+  def page_title(:new, _assigns), do: gettext("New individual")
   def page_title(_, _), do: gettext("Individuals")
-  def page_title(:new, assigns), do: gettext("New individual")
 end
